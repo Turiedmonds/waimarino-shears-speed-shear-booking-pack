8d732279aade7f6841a14558dc9691afa20853e9
Turiedmonds <turi-edmonds@hotmail.com>
2026-08-21 01:39:44 +1200

    Bump final booking polish loader version

--- straight-final.js
+++ straight-final.js
@@ -8,7 +8,7 @@
     hireOptions.async = false;
     hireOptions.onload = () => {
       const finalPolish = document.createElement('script');
-      finalPolish.src = 'hire-options-final-polish.js?v=1.0.0';
+      finalPolish.src = 'hire-options-final-polish.js?v=1.0.1';
       finalPolish.async = false;
       finalPolish.onload = () => {
         const emailOptions = document.createElement('script');
