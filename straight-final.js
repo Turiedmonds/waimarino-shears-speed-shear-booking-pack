0a5a0b875560420344a87757fc20490cb099a775
Turiedmonds <turi-edmonds@hotmail.com>
2026-08-27 12:11:36 +1200

    Load current hire options version

--- straight-final.js
+++ straight-final.js
@@ -8,7 +8,7 @@
     initialCopySync.async = false;
     initialCopySync.onload = () => {
       const hireOptions = document.createElement('script');
-      hireOptions.src = 'hire-options.js?v=1.0.0';
+      hireOptions.src = 'hire-options.js?v=1.0.1';
       hireOptions.async = false;
       hireOptions.onload = () => {
         const finalPolish = document.createElement('script');
